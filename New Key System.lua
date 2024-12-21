97ac5f2b613469a1e8430dc5c9c60283441c8944
kenshidoko <117933685+kenshidoko@users.noreply.github.com>
2024-12-22 04:29:40 +0800

    Update New Key System.lua

--- New Key System.lua
+++ New Key System.lua
@@ -1,6 +1,6 @@
 local KeyGuardLibrary = loadstring(game:HttpGet("https://cdn.keyguardian.org/library/v1.0.0.lua"))()
-local trueData = "e3429ccc46134a3a80db0a3f87a56dbf"
-local falseData = "04564cb447ce4d3dbca64d07f1c8ed89"
+local trueData = "30e0e53ff4d344de9baf2063562aee4b"
+local falseData = "17c5e881ecb24e578edaf1dd13fa5f18"
 
 KeyGuardLibrary.Set({
   publicToken = "ac92238a1e2e4ea5abfa5c40a24ea39b",
@@ -9,23 +9,55 @@ KeyGuardLibrary.Set({
   falseData = falseData,
 })
 
-local key = "test"
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local key = ""
 
-local getkey = KeyGuardLibrary.getLink()
-print(getkey)
+local Window = Fluent:CreateWindow({
+    Title = "Key System",
+    SubTitle = "Lycusss HUB",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(580, 340),
+    Acrylic = false,
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.LeftControl
+})
 
-local response = KeyGuardLibrary.validateDefaultKey(key)
-print(response)
+local Tabs = {
+    KeySys = Window:AddTab({ Title = "Key System", Icon = "key" }),
+}
 
-if response == trueData then
-  print("Key is valid")
-else
-  print("Key is invalid")
-end
+local Entkey = Tabs.KeySys:AddInput("Input", {
+    Title = "Enter Key",
+    Description = "Enter Key Here",
+    Default = "",
+    Placeholder = "Enter key…",
+    Numeric = false,
+    Finished = false,
+    Callback = function(Value)
+        key = Value
+    end
+})
 
---[[
-  KeyGuardLibrary.validateDefaultKey(key) - Validate key
-  KeyGuardLibrary.validatePremiumKey(key) - Validate premium key
-  KeyGuardLibrary.getService() - Get service
-  KeyGuardLibrary.getLink() - Get link
-]]
+local Checkkey = Tabs.KeySys:AddButton({
+    Title = "Check Key",
+    Description = "Enter Key before pressing this button",
+    Callback = function()
+        local response = KeyGuardLibrary.validateDefaultKey(key)
+        if response == trueData then
+           print("Key is valid")
+           -- Your code here
+        else
+           print("Key is invalid")
+        end
+    end
+})
+
+local Getkey = Tabs.KeySys:AddButton({
+    Title = "Get Key",
+    Description = "Get Key here",
+    Callback = function()
+       setclipboard(KeyGuardLibrary.getLink())
+    end
+})
+
+Window:SelectTab(1)
