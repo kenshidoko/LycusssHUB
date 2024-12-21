8f3d11b18ee7399e2852d10e2a76a0390c2a0e90
kenshidoko <117933685+kenshidoko@users.noreply.github.com>
2024-12-22 03:37:31 +0800

    Add files via upload

---
+++ Lycusss Hub V2.lua
@@ -0,0 +1,55 @@
+
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+   Name = "Lycusss Hub V2",
+   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   LoadingTitle = "Loading...",
+   LoadingSubtitle = "Made by Lycusss",
+   Theme = "Amethyst", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "Lycusss Hub"
+   },
+
+   Discord = {
+      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+
+   KeySystem = true, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Lycusss HUB",
+      Subtitle = "Key System",
+      Note = "Get the key from my Latest Video", -- Use this to tell the user how to get a key
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"https://pastebin.com/KnyZ3uyf"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
+})
+
+Rayfield:Notify({
+   Title = "Youtube Channel",
+   Content = "Lycusss",
+   Duration = 6.5,
+   Image = 4483362458,
+})
+
+local BloxFruitTab = Window:CreateTab("Blox Fruit", 4483362458)
+
+local Section = BloxFruitTab:CreateSection("BloxFruit Scripts")
+
+local Button = BloxFruitTab:CreateButton({
+   Name = "SPEEDY X",
+   Callback = function()
+   loadstring(game:HttpGet("https://rawscripts.net/raw/UPD-Fisch-Auto-farm-dupe-caught-and-more-23087"))()
+   end,
+})
+
