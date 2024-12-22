62eeaa5f7d29b2d3c2d034502ec3c13dfee1ef20
kenshidoko <117933685+kenshidoko@users.noreply.github.com>
2024-12-23 02:51:29 +0800

    Update New Key System.lua

--- New Key System.lua
+++ New Key System.lua
@@ -1,7 +1,8 @@
-local KeyGuardLibrary = loadstring(game:HttpGet("https://cdn.keyguardian.org/library/v1.0.0.lua"))()
-local trueData = "30e0e53ff4d344de9baf2063562aee4b"
-local falseData = "17c5e881ecb24e578edaf1dd13fa5f18"
+local startTime = os.clock()
 
+local KeyGuardLibrary = loadstring(game:HttpGet("https://cdn.keyguardian.org/library/v1.0.0.lua"))()
+local trueData = "333f33cebe1649a8a74d731605b2522f"
+local falseData = "4413a8e451f54da58cba28369b0a3417"
 
 KeyGuardLibrary.Set({
   publicToken = "ac92238a1e2e4ea5abfa5c40a24ea39b",
@@ -30,56 +31,58 @@ if isfile(Directory) then
     end
 else
     local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
-    local key = "8951bb74939c4c2fa879b7cd887b37f2"
-()
-local Window = Fluent:CreateWindow({
-    Title = "Key System",
-    SubTitle = "Lycusss HUB",
-    TabWidth = 160,
-    Size = UDim2.fromOffset(580, 340),
-    Acrylic = false,
-    Theme = "Dark",
-    MinimizeKey = Enum.KeyCode.LeftControl
-})
 
-local Tabs = {
-    KeySys = Window:AddTab({ Title = "Key System", Icon = "key" }),
-}
+    local Window = Fluent:CreateWindow({
+        Title = "Lycuss HUB V2",
+        SubTitle = "Key System",
+        TabWidth = 160,
+        Size = UDim2.fromOffset(520, 320),
+        Acrylic = false,
+        Theme = "Dark",
+        MinimizeKey = Enum.KeyCode.LeftControl
+    })
 
-local Entkey = Tabs.KeySys:AddInput("Input", {
-    Title = "Enter Key",
-    Description = "Enter Key Here",
-    Default = "",
-    Placeholder = "Enter key…",
-    Numeric = false,
-    Finished = false,
-    Callback = function(Value)
-        key = Value
-    end
-})
+    local Tabs = {
+        KeySys = Window:AddTab({ Title = "Key System", Icon = "key" }),
+    }
 
-local Checkkey = Tabs.KeySys:AddButton({
-    Title = "Check Key",
-    Description = "Enter Key before pressing this button",
-    Callback = function()
-        local response = KeyGuardLibrary.validateDefaultKey(key)
-        if response == trueData then
-           print("Key is valid")
-           writefile(Directory, InputKey.Value)
-           loadstring(game:HttpGet("https://raw.githubusercontent.com/kenshidoko/LycusssHUB/refs/heads/main/Hub.lua"))()
-           Window:Destroy()
-        else
-           print("Key is invalid")
+    local InputKey = Tabs.KeySys:AddInput("InputKey", {
+        Title = "Input Key",
+        Description = "Insert your key...",
+        Default = "",
+        Placeholder = "Enter keyâ€¦",
+        Numeric = false,
+        Finished = false
+    })
+
+    local Checkkey = Tabs.KeySys:AddButton({
+        Title = "Check Key",
+        Description = "Enter Key before pressing this button",
+        Callback = function()
+            print(InputKey.Value)
+            local response = KeyGuardLibrary.validateDefaultKey(InputKey.Value)
+            if response == trueData then
+               print("Key is valid")
+               loadstring(game:HttpGet("https://raw.githubusercontent.com/kenshidoko/LycusssHUB/refs/heads/main/Hub.lua"))()
+               writefile(Directory, InputKey.Value)
+               Window:Destroy()
+            else
+               print("Key is invalid")
+            end
         end
-    end
-})
+    })
 
-local Getkey = Tabs.KeySys:AddButton({
-    Title = "Get Key",
-    Description = "Get Key here",
-    Callback = function()
-       setclipboard(KeyGuardLibrary.getLink())
-    end
-})
+    local Getkey = Tabs.KeySys:AddButton({
+        Title = "Get Key",
+        Description = "Get Key here",
+        Callback = function()
+           setclipboard(KeyGuardLibrary.getLink())
+        end
+    })
 
-Window:SelectTab(1)
+    Window:SelectTab(1)
+
+    local endTime = os.clock()
+    local loadTime = endTime - startTime
+    print("Time taken to load everything: " .. loadTime .. " seconds")
+end
